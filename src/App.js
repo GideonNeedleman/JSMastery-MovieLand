3ee643153c008d7442f672732bc298599ec8d903
Gideon Needleman <111554925+GideonNeedleman@users.noreply.github.com>
2023-07-23 17:26:32 -0700

    Enable search box

--- src/App.js
+++ src/App.js
@@ -5,16 +5,9 @@ import MovieCard from "./MovieCard";
 
 const API_URL = 'https://www.omdbapi.com/?apikey=68ecbe5d'
 
-/* const movie = {
-  "Title": "Spiderman in Cannes",
-  "Year": "2016",
-  "imdbID": "tt5978586",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg"
-} */
-
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`)
@@ -33,13 +26,13 @@ const App = () => {
 
       <div className="search">
         <input placeholder="Search for movies"
-        value="spiderman"
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <img
         src={SearchIcon}
         alt="search"
-        onClick={() => {}}
+        onClick={() => searchMovies(searchTerm)}
       />
       </div>
 
